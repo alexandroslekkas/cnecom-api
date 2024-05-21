8f8e324cb50d57827f4e680ad7b643da17090717
Alexandros Lekkas <alexandros.lekkas@outlook.com>
2024-05-21 18:24:51 +0300

    Initilization

---
+++ src/main/java/online/cnecom/cnecomapi/CNEcomAPIApplication.java
@@ -0,0 +1,13 @@
+package online.cnecom.cnecomapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CNEcomAPIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CNEcomAPIApplication.class, args);
+	}
+
+}
---
+++ src/test/java/online/cnecom/cnecomapi/CnecomApiApplicationTests.java
@@ -0,0 +1,13 @@
+package online.cnecom.cnecomapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnecomApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
