cf7fdd2223f3537e567c776cabf4ddabbc67b2a2
Alexandros Lekkas <alexandros.lekkas@outlook.com>
2024-05-23 01:33:25 +0300

    More friendly structure for FlutterFlow

--- src/main/java/online/cnecom/cnecomapi/taobao/TaobaoController.java
+++ src/main/java/online/cnecom/cnecomapi/taobao/TaobaoController.java
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.List;
-
 @RestController
 public class TaobaoController {
     private final RestTemplate restTemplate;
